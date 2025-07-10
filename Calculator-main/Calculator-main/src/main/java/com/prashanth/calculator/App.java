e3ca0cf1c27c928a919b7c779cb8cd742791e122
yarlagaddaprashanth <yarlagaddaprashanth333@gmail.com>
2025-07-11 00:17:36 +0530

    Add files via upload

---
+++ Calculator-main/Calculator-main/src/main/java/com/prashanth/calculator/App.java
@@ -0,0 +1,10 @@
+package com.prashanth.calculator;
+
+import com.prashanth.calculator.ui.CalculatorUI;
+
+public class App {
+
+    public static void main(String[] args) {
+        new CalculatorUI();
+    }
+}
